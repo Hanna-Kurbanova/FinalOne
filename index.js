a21489009705f478c66dd6b3bbd45a3558345baa
Hanna Kurbanova <129006088+Hanna-Kurbanova@users.noreply.github.com>
2023-06-01 12:37:32 +0100

    Add JS forecast + new api

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.dat.daily);
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -54,8 +54,8 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "77df4efc31ae8b0acc6c7d09570dde67";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
